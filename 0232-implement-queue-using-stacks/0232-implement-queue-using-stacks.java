6bdf895a1dd280d5a69026b29a3bed33881e268f
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-01 00:08:20 +0530

    Time: 0 ms (100%), Space: 42.5 MB (83.98%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,66 @@
+class MyQueue {
+    Stack<Integer> st = new Stack<>();
+    Stack<Integer> helper = new Stack<>();
+
+    public MyQueue() {
+        
+    }
+
+    public void push(int x) { // 0(n)
+        while(st.size() > 0){
+            helper.push(st.pop());
+        }
+        st.push(x);
+        while(helper.size() > 0){
+            st.push(helper.pop());
+        }
+    }
+    
+    
+    public int pop() { //0(1)
+        return st.pop();
+    }
+    
+    public int peek() { // 0(1)
+      return st.peek();  
+    }
+    
+    // public void push(int x) { // 0(1)
+    //     st.push(x);
+    // }
+    
+    // public int pop() { //0(n)
+    //     while(st.size() > 1){
+    //         helper.push(st.pop());
+    //     }
+    //     int front = st.pop();
+    //     while(helper.size() > 0){
+    //         st.push(helper.pop());
+    //     }
+    //     return front;
+    // }
+    
+    // public int peek() {  // 0(n)
+    //     while(st.size() > 1){
+    //         helper.push(st.pop());
+    //     }
+    //     int front = st.peek();
+    //     while(helper.size() > 0){
+    //         st.push(helper.pop());
+    //     }
+    //     return front;
+    // }
+    
+    public boolean empty() {
+        return st.size() == 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */

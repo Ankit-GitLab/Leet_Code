f21c6219c84ac3558fe591d6b98de3515f266a39
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-06 18:33:14 +0530

    Time: 3 ms (98.54%), Space: 84.7 MB (56.95%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -5,11 +5,26 @@ class Solution {
             int first = arr[i][0];
             int last = arr[i][1];
             int seats = arr[i][2];
-            for(int j=first-1; j<=last-1; j++){
-                ans[j] += seats;
-            }
-
+            ans[first-1] += seats;
+            if(last<n) ans[last] -= seats; 
         }
-        return ans;
+        for(int i=1; i<ans.length; i++){
+            ans[i] += ans[i-1];
+        }
+        return ans; // 748ms
     }
+
+    // public int[] corpFlightBookings(int[][] arr, int n) {
+    //     int[] ans = new int[n];
+    //     for(int i=0; i<arr.length; i++){
+    //         int first = arr[i][0];
+    //         int last = arr[i][1];
+    //         int seats = arr[i][2];
+    //         for(int j=first-1; j<=last-1; j++){
+    //             ans[j] += seats;
+    //         }
+
+    //     }
+    //     return ans; // 748ms
+    // }
 } 

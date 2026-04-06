61ac3a07e0a9f36c29cd296955d3ca6d8c22e2c2
Ankit Kumar <ankit_246003@saitm.ac.in>
2026-04-06 13:14:49 +0530

    Time: 2 ms (94.98%), Space: 72 MB (56.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -8,17 +8,13 @@ class Solution {
             pre[i] = arr[i-1] * pre[i-1];
         }
 
-        int[] suf = new int[n];
-        suf[n-1] = 1;
+      
+        int suf = 1;
         for(int i = n-2; i >= 0; i--){
-            suf[i] = arr[i+1] * suf[i+1];
+            suf *= arr[i+1];
+            pre[i] *= suf;
         }
 
-        
-        for(int i = 0; i < n; i++){
-            suf[i] *= pre[i];
-        }
-
-        return suf;
+        return pre;
     }
 }
